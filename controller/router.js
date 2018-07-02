8f927e1499fe2685f1e6117a122904b37035bcc1
maple.p <maple.p@anvo.com>
2018-07-02 11:16:01 +0800

    submit one new branch

--- controller/router.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router")();
-const api = require('./api');
-Router.get("/", api.showIndex)
-  .post("/postForm", api.postForm)
-  .get("/getData", api.getTableData)
-  .del("/delData", api.delTableData)
-  .post("/upDate", api.updateTableData);
-module.exports = Router;
